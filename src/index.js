a1580cd93f41c0f5b63b767650fed41ff039c302
anurag <mikkydhiman@gmail.com>
2019-05-28 23:10:01 +0530

    Chapter 1 to 7

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'; // Here {react} is module and {React} is object;
+import ReactDOM from 'react-dom'; // same concept goes here......
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// console.log("The type of the react is", typeof React);
+// console.log("The type of the react is", typeof ReactDOM);
+// console.log("The ReactDOM I am getting is", ReactDOM);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const element = <h1> This is the React element </h1>
+
+//console.log(typeof element);
+
+// To render this above element in the DOM
+
+console.log("The mounting function I am getting is ", ReactDOM.render(element, document.getElementById('root')));
