6d45f826c16a550e42539b800381ee7efeb54316
anurag <mikkydhiman@gmail.com>
2019-05-30 22:52:42 +0530

    Components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react'; // Here {react} is module and {React} is object;
-import ReactDOM from 'react-dom'; // same concept goes here......
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counter';
+ReactDOM.render(<Counter />, document.getElementById('root'));
 
-// console.log("The type of the react is", typeof React);
-// console.log("The type of the react is", typeof ReactDOM);
-// console.log("The ReactDOM I am getting is", ReactDOM);
-
-const element = <h1> This is the React element </h1>
-
-//console.log(typeof element);
-
-// To render this above element in the DOM
-
-console.log("The mounting function I am getting is ", ReactDOM.render(element, document.getElementById('root')));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
