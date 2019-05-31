6bb581e2cc2ff782258880e022cbe8d3ed54a2b9
anurag <mikkydhiman@gmail.com>
2019-06-01 01:48:15 +0530

    Rendering the components with different conditions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,18 +3,34 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
+        tags: []
+    };
+
+    renderTags() {
+        if (this.state.tags.length === 0) return <p> There are no tags</p>;
+        return <ul> {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
     }
+
     render() {
+        console.log(" This value of this is", this)
         return (
             <div>
-                <h1> {this.formatCount()} </h1>
-                <button>Increment</button>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
+                {this.state.tags.length === 0 && "Please create a new tag!"}
+                {this.renderTags()}
             </div>
 
         )
     }
 
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+    // this is the logic for the element
     formatCount() {
         const { count } = this.state;
 
